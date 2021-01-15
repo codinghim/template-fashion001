d742efb7c1d89116720c0c013976cd41e36a61a0
Anderson Yoon <snowglow22y@gmail.com>
2021-01-14 21:02:21 -0500

    test 3

--- pages/lookbook.js
+++ pages/lookbook.js
@@ -172,7 +172,7 @@ export default function Lookbook() {
                 whileHover="hover"
                 className={image.style}>
                 <Image
-                  priority="true"
+                  priority
 
                   alt={`image${i}`}
                   src={image.source}
